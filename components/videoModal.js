2b0e52dae536079001e9bf231959e6d59bf793f1
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2022-12-20 11:47:50 +0900

    Update videoModal.js

--- components/videoModal.js
+++ components/videoModal.js
@@ -6,13 +6,21 @@ import { Modal, Link } from "@nextui-org/react";
 const VideoModal = ({ isVisible, onClose, user, usertoken }) => {
 
 	if(!isVisible) return null;
+	
+	/* AWS 설정 객체 업데이트 */
+	AWS.config.update({
+		region: "ap-northeast-2", // 버킷이 존재하는 리전
+		credentials: new AWS.CognitoIdentityCredentials({
+		  IdentityPoolId: process.env.NEXT_PUBLIC__AWS_CONFIG, // cognito 인증 풀에서 받아온 키
+		}),
+	})
 
     return (
 		<div  onContextMenu={e => e.preventDefault()}>
 			<Modal css={{background:"transparent",}} noPadding open={isVisible} onClose={onClose} width={335} height={624} animated={false}>
 				<div className="inset-0 bg-opacity-75 flex justify-center items-center z-0 overflow-scroll bg-cover bg-scroll h-full w-full">
 					<video
-                        src="/video/testVideo.mp4"
+                        src="https://advent-reindeer-test.s3.ap-northeast-2.amazonaws.com/testVideo.mp4"
                         width="335"
                         height="624"
                         controls="controls"
